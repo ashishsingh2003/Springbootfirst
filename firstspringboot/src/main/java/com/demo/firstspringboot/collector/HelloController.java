53bf9634372ca2efc6ae9a1060e9e1038de3cb1f
Ashish Singh <ashishsinghrana39@gmail.com>
2025-03-01 21:16:52 +0530

    UC2 committed

--- firstspringboot/src/main/java/com/demo/firstspringboot/collector/HelloController.java
+++ firstspringboot/src/main/java/com/demo/firstspringboot/collector/HelloController.java
@@ -2,6 +2,7 @@ package com.demo.firstspringboot.collector;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
@@ -10,5 +11,14 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+}
+@RestController
+@RequestMapping("/hello")
+class HelloControllerMark {
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "Mark") String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
